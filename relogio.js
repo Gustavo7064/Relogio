ef5e523dd12745899ab57df5583bdd0c74d2c36d
Gustavo Goes <gustavorochamacie12007@gmail.com>
2026-02-27 12:45:59 -0300

    criando arrays, e finalizando js

--- relogio.js
+++ relogio.js
@@ -18,6 +18,36 @@ function Repete(){
     if(m<10){
         m="0"+ m;
     }
+ let a =hoje.getFullYear();
 
-    
+    var semana = new Array();
+    semana[0] ="Domingo";
+    semana[1] ="Segunda-feira";
+    semana[2] ="Terça-feira";
+    semana[3] ="Quarta-feira";
+    semana[4] ="Quinta-feira";
+    semana[5] ="Sexta-feira";
+    semana[6] ="Sabádo";
+
+    let h =hoje.getHours();
+    if(h<10){
+        h="0"+ h;
+    }
+    let mn = hoje.getMinutes();
+    if(mn<10){
+        mn="0"+ mn;
+    }
+    let s = hoje.getSeconds();
+    if(s<10){
+        s="0"+ s;
+    }
+
+    document.getElementById("horario").value ="Hoje é dia " + d + "/"+ m + "/" + a + ", " + semana[1]+ " e o horário é "+ h +":"+ mn + ":"+ s;
+
+} 
+
+function Limpar()
+{
+  document.getElementById("horario").value="";
+  clearInterval(intervalo);
 }
